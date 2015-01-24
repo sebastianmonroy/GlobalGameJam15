cfd67ffe6fa0f6b270c066768b5ae9884cfbe15b
Sebastian Monroy <sebastian.monroy@gmail.com>
2015-01-24 12:30:53 -0500

    AJ Polygon Editor!

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shape 
+{
+	public Vector2 center;
+	public List<Vertex> vertices = new List<Vertex>();
+	
+}
---
+++ Assets/Scripts/Vertex.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Vertex
+{
+	public Vector2 position;	// relative position from center of shape
+}
