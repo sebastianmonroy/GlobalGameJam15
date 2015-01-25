b69c5c2ccde17715a9be8e89b82d1f07d2818a52
mbhuet <mbhuet@gmail.com>
2015-01-25 12:53:11 -0500

    sexy sexy ice cream

--- Assets/Scripts/State Managers/SexStateManager.cs
+++ Assets/Scripts/State Managers/SexStateManager.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 
 public class SexStateManager : FrameStateManager {
 	
-	SimpleState sexState, finishedState;
+	SimpleState sexState, finishedState1, finishedState2;
 	public GameObject MaleRoot, FemaleRoot;
+
+	float maleP = 1;
+	float femaleP = 0;
+
+	Vector3 dragOffset;
 	
 	void Start() 
 	{
+		//Debug.Log (GetClosestPointOnLine(Vector3.zero, Vector3.up * 5, new Vector3(.5f,.5f,0)));
 		sexState = new SimpleState(sexEnter, sexUpdate, sexExit, "SEX");
-		finishedState = new SimpleState(finishedEnter, finishedUpdate, finishedExit, "FINISHED");
-		
+		finishedState1 = new SimpleState(finishedEnter, finishedUpdate, finishedExit, "FINISHED1");
+		finishedState2 = new SimpleState(finishedEnter, finishedUpdate, finishedExit, "FINISHED2");
+
 		stateMachine.SwitchStates(sexState);
 	}
 	
@@ -33,22 +40,89 @@ public class SexStateManager : FrameStateManager {
 		{
 			if (f.hitObject != null)
 			{
-				if (f.hitObject.transform.parent.gameObject == MaleRoot)
-				{
-					MaleRoot.transform.position = new Vector3(f.GetWorldPosition().x, f.GetWorldPosition().y, MaleRoot.transform.position.z);
-				} 
-				else if (f.hitObject.transform.parent.gameObject == FemaleRoot)
-				{
-					FemaleRoot.transform.position = new Vector3(f.GetWorldPosition().x, f.GetWorldPosition().y, FemaleRoot.transform.position.z);
+				if (f.hitObject.tag == "GenderSymbol"){
+
+					Vector3 lineStart = f.hitObject.transform.parent.FindChild("lineStart").transform.position;
+					Vector3 lineEnd = f.hitObject.transform.parent.FindChild("lineEnd").transform.position;
+
+					Vector3 pos = GetClosestPointOnLine(lineStart, lineEnd, new Vector3(f.GetWorldPosition().x, f.GetWorldPosition().y, f.hitObject.transform.position.z));
+					pos.x = Mathf.Clamp(pos.x, Mathf.Min(lineStart.x, lineEnd.x), Mathf.Max(lineStart.x, lineEnd.x));
+					pos.y = Mathf.Clamp(pos.y, Mathf.Min(lineStart.y, lineEnd.y), Mathf.Max(lineStart.y, lineEnd.y));
+
+					f.hitObject.transform.position = pos;
+
+					float percent = ((pos - lineStart).magnitude)/((lineEnd-lineStart).magnitude);
+
+					Transform left = f.hitObject.transform.FindChild("arrow 2");
+					Transform right = f.hitObject.transform.FindChild("arrow 3");
+
+					Vector3 rot = Vector3.zero;
+					rot.z = -45f*(percent+.01f);
+					//Debug.Log(percent+.001f);
+
+
+					left.localEulerAngles = rot;
+					right.localEulerAngles = -rot;
+
+					Color c = Color.Lerp(new Color(255f/255f,173f/255f,154f/255f,1), new Color(150f/255f,177f/255f,255f/255f,1), percent);
+
+					MeshRenderer[] rends = f.hitObject.transform.parent.GetComponentsInChildren<MeshRenderer>();
+					foreach (MeshRenderer rend in rends){
+						rend.material.color = c;
+
+						//Debug.Log(c);
+					}
+
+//					Debug.Log(f.hitObject.transform.parent);
+					if (f.hitObject.transform.parent.name == "Male"){
+						maleP = percent;
+					}
+					else if (f.hitObject.transform.parent.name == "Female"){
+						femaleP = percent;
+					}
+
 				}
+
+				else if (f.hitObject == Polygons[0] || f.hitObject == Polygons[1]){
+					f.hitObject.transform.parent.position = new Vector3(f.GetWorldPosition().x, f.GetWorldPosition().y, f.hitObject.transform.parent.position.z);
+
+				}
+			
+
+
+
+
+
+
 			}
 		}
 		
 		if (Vector3.Distance(MaleRoot.transform.position, FemaleRoot.transform.position) <= 1f)
 		{
-			//stateMachine.SwitchStates(finishedState);
+			Debug.Log(maleP + femaleP);
+
+			if (maleP + femaleP > .5f && maleP + femaleP < 1.5f){
+				stateMachine.SwitchStates(finishedState1);
+			}
+		else {
+				stateMachine.SwitchStates(finishedState2);
+
+			}
+
 		}
 	}
+
+	static public Vector3 GetClosestPointOnLine(Vector3 start, Vector3 end, Vector3 point){
+		Vector3 ab = end - start;
+		ab = ab.normalized;
+		Vector3 ap = point - start;
+
+		Vector3 x = start + (Vector3.Dot (ap, ab)) * ab.normalized;
+
+		return x;
+	}
+
+
 	void sexExit() {}
 	#endregion
 	
