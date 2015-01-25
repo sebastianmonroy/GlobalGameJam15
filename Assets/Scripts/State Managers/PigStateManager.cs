97ca3b1c10c29f9294bfe7b13e585f5afc779152
Sebastian Monroy <sebastian.monroy@gmail.com>
2015-01-25 14:25:01 -0500

    starting mainstatemachine

---
+++ Assets/Scripts/State Managers/PigStateManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PigStateManager : FrameStateManager {
+
+	SimpleState idleState, finishedState;	
+	
+	void Start() 
+	{
+		idleState = new SimpleState(IdleEnter, IdleUpdate, IdleExit, "IDLE");
+		finishedState = new SimpleState(null, null, null, "FINISHED");
+
+		stateMachine.SwitchStates(idleState);
+	}
+	
+	#region IDLE
+	void IdleEnter() 
+	{
+		
+	}
+	
+	void IdleUpdate() 
+	{
+		
+	}
+	
+	void IdleExit() {}
+	#endregion
+	
+}
